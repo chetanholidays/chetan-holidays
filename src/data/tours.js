9c53ec594bed69902472a06812d27a705fc17bd2
Chetan Holidays <chetanholidays99@gmail.com>
2026-07-07 23:29:08 +0530

    Shared layout and tours page working

---
+++ src/data/tours.js
@@ -0,0 +1,26 @@
+export const tours = [
+  {
+    title: "Kashmir Tour",
+    image: "/images/kashmir.jpg",
+    duration: "6 Nights / 7 Days",
+    badge: "Best Seller",
+    price: "Contact Us",
+    slug: "kashmir"
+  },
+  {
+    title: "Kerala Tour",
+    image: "/images/keralam.jpg",
+    duration: "5 Nights / 6 Days",
+    badge: "Family Favourite",
+    price: "Contact Us",
+    slug: "kerala"
+  },
+  {
+    title: "Dubai Tour",
+    image: "/images/dubai.jpg",
+    duration: "4 Nights / 5 Days",
+    badge: "International",
+    price: "Contact Us",
+    slug: "dubai"
+  }
+];
